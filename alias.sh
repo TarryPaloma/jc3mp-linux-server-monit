31b81dcebba1f9d8396e7061d0bb72dbc6a5c401
TarryPaloma <beyer.dynamics@gmail.com>
2017-07-22 19:53:21 +0100

    changed to account for monit files being moved to a ./monit directory

--- alias.sh
+++ alias.sh
@@ -2,6 +2,7 @@
 
 NAME='replaceme' #GET THIS FROM MAIN SCRIPT
 DIR=/home/'replaceme'/jc3mp/'replaceme' #GET USERNAME FROM MAIN SCRIPT!
+MONITDIR="$DIR/monit"
 LOG_FILE="$NAME.log"
 CONSOLE_LOG="logs/console.log"
 PID_FILE="$NAME.pid"
@@ -12,26 +13,26 @@ VAR=`ps -ef | grep "$DIR$CMD" | grep -v grep | wc -l`
 if [ $VAR -gt 0 ]; then
 echo "$NAME already running..."
 else
-rm -f $DIR$PID_FILE
+rm -f $MONITDIR$PID_FILE
 PIDSV=0
 PPIDSH=0
 echo $(date +"%T-%d\%m\%Y") >> $DIR$LOG_FILE
 echo "$NAME not running..."
-echo "$DIR ... $PID_FILE" >> $DIR$LOG_FILE
-screen -dm sh -c 'echo $$ > '$DIR$PID_FILE' ; cd '$DIR' ; $CMD | tee -a '$DIR$CONSOLE_LOG
-while [ ! -s $DIR$PID_FILE ]; do sleep 1; done
-PPIDSH=`cat $DIR$PID_FILE`
-echo "PPID is $PPIDSH" >> $DIR$LOG_FILE
+echo "$DIR ... $PID_FILE" >> $MONITDIR$LOG_FILE
+screen -dm sh -c 'echo $$ > '$MONITDIR$PID_FILE' ; cd '$DIR' ; $CMD | tee -a '$DIR$CONSOLE_LOG
+while [ ! -s $MONITDIR$PID_FILE ]; do sleep 1; done
+PPIDSH=`cat $MONITDIR$PID_FILE`
+echo "PPID is $PPIDSH" >> $MONITDIR$LOG_FILE
 PIDSV=$(pgrep -P $PPIDSH -x Server)
-echo "PID is $PIDSV" >> $DIR$LOG_FILE
-echo $PIDSV > $DIR$PID_FILE
+echo "PID is $PIDSV" >> $MONITDIR$LOG_FILE
+echo $PIDSV > $MONITDIR$PID_FILE
 echo "$NAME listener is started..."
 fi
 }
 
 function stopnotification {
-kill `cat $DIR$NAME.pid`
-rm -f $DIR$PID_FILE
+kill `cat $MONITDIR$NAME.pid`
+rm -f $MONITDIR$PID_FILE
 echo "$NAME listener stopped."
 }
 case $1 in
